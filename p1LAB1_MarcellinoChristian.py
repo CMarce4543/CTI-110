34a045e226f90003f3e129b551849975ed3b5abb
CMarce4543 <160330427+CMarce4543@users.noreply.github.com>
2024-02-18 15:18:49 -0500

    Add files via upload
    P1LAB1

---
+++ p1LAB1_MarcellinoChristian.py
@@ -0,0 +1,9 @@
+Python 3.12.2 (tags/v3.12.2:6abddd9, Feb  6 2024, 21:26:36) [MSC v.1937 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/chris/Downloads/CTI120/CTI110/p1LAB1_MarcellinoChristian.py
+Christian Marcellino
+02/17/2024
+P1LAB1
+Hey John!
+Welcome to Python

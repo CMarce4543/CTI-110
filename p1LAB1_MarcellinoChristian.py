3f1f3966a231b50b47f9047c8a83ad5ec37bef43
CMarce4543 <160330427+CMarce4543@users.noreply.github.com>
2024-02-24 17:02:25 -0500

    Update p1LAB1_MarcellinoChristian.py

--- p1LAB1_MarcellinoChristian.py
+++ p1LAB1_MarcellinoChristian.py
@@ -1,9 +1,11 @@
-Python 3.12.2 (tags/v3.12.2:6abddd9, Feb  6 2024, 21:26:36) [MSC v.1937 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> 
-= RESTART: C:/Users/chris/Downloads/CTI120/CTI110/p1LAB1_MarcellinoChristian.py
-Christian Marcellino
-02/17/2024
-P1LAB1
-Hey John!
-Welcome to Python
+# Christian Marcellino
+
+ # 02/24/2024
+
+ # P1LAB1
+
+ # Program will display first name and a greeting.
+
+print('Enter your first name:', end=' ')
+first_name = input()
+print('Welcome to zyBook!', first_name)
